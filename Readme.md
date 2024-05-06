c4d96cf57942602efdf46d912d1dba03d8b7993e
Ruszrok <judanov.v@gmail.com>
2024-05-06 22:23:52 +0100

    Updated

---
+++ Readme.md
@@ -0,0 +1,10 @@
+## This is aggregator for Levels fyi salaries data
+
+
+
+Usage:
+1. Navigate to [level.fyi](https://www.levels.fyi/t/software-engineer/)
+2. Filter the data as you wanted
+3. Copy tables and insert them inside main.html body tag
+4. Check row_class, level_class, salary_class, company_class. It is obfuscated and probably should be mannualy updated.
+5. Parse data and operate it
---
+++ parse.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import sys
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: python parse.py <html_file>')
+        sys.exit(1) 
+
+    file_name = sys.argv[1]
+    with open('main.html', 'r') as file:
+        html = file.read()
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    row_class = "salary-row_collapsedSalaryRow__IQ3om"
+    level_class = "salary-row_levelName____tz6"
+    salary_class = "salary-row_totalCompCell__553Rk"
+    company_class = "salary-row_companyName__obLh0"
+
+    rows = soup.find_all('tr', class_=row_class)
+    print(f'Level, Company, Salary')
+    for row in rows:
+        level = row.find('p', class_=level_class).text
+        salary = 'Unknown'
+        salary_td = row.find('td', class_=salary_class)
+        if salary_td:
+            salary = salary_td.find('p').text
+            
+        company = row.find('a', class_=company_class).text if row.find('a', class_=company_class) else 'Unknown'
+        
+        #Clean level from multiple spaces and new lines
+        level = ' '.join(level.split())
+        print(f'{level}, {company.strip()}, "{salary.strip()}"')
+    
+
